f3b14e0deba6847a353d6cb0ce17bc5d64113a25
Rafael Soares <85128097+Ochernobas@users.noreply.github.com>
2025-03-13 14:47:27 -0300

    b

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&redirect_uri=<REDIRECT_URL>&scope=<SCOPE>%20<SCOPE>&response_type=token&state=123
 */
 
+//FLAMENGOO
+
 const Home = () => {
     const client_id = "b4197f508abe44bfa68e5197369cf427"
     const spotify_authorize_endpoint = "https://accounts.spotify.com/authorize"
@@ -13,7 +15,7 @@ const Home = () => {
     const scopes_url_param = scopes.join(space_delimiter)
 
     const handleLogin = () => {
-        window.location = `${spotify_authorize_endpoint}?client_id=${client_id}&redirect_uri=${redirect_utl_after_login}&scope=${scopes_url_param}&response_type=token&show_dialog=true`
+        window.location = `${spotify_authorize_endpoint}?client_id=${client_id}&scope=${scopes_url_param}&response_type=token&show_dialog=true&redirect_uri='${redirect_utl_after_login}'`
     }
 
   return (
